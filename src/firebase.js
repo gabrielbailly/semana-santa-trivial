8b8f387b153d495e6159279a574173aa8fb06295
gabrielbailly <158313900+gabrielbailly@users.noreply.github.com>
2026-03-18 22:33:21 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "semana-santa-2026.firebaseapp.com",
+  projectId: "semana-santa-2026",
+  storageBucket: "semana-santa-2026.firebasestorage.app",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
